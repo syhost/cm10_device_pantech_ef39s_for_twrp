fd2a69154732a514ce464158eae8afa23d7e0802
syhost <syhost@163.com>
2012-12-05 03:40:42 +0800

    first commit

---
+++ cm.mk
@@ -0,0 +1,13 @@
+
+# Release name
+PRODUCT_RELEASE_NAME := ef39s
+
+# Inherit device configuration
+$(call inherit-product, device/pantech/ef39s/device_ef39s.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ef39s
+PRODUCT_NAME := cm_ef39s
+PRODUCT_BRAND := pantech
+PRODUCT_MODEL := ef39s
+PRODUCT_MANUFACTURER := pantech
---
+++ device_ef39s.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+LOCAL_PATH := device/pantech/ef39s
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_KERNEL):kernel
+	
